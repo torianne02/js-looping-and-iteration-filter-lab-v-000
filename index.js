add77ce18f106f34779bfc1ee13f3c8859786b55
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-09-18 23:43:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Code your solution in this file
+function findMatching(drivers, name) {
+  return list.filter(function (driverName) {
+    return driverName.toLowerCase() === name.toLowerCase();
+  });
+}
