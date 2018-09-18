0b525f62b5864c8992afbe53970bc23b80df0967
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-09-18 23:43:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-function findMatching(list, name) {
-  return list.filter(function (driverName) {
+function findMatching(driver, name) {
+  return driver.filter(function (driverName) {
     return driverName.toLowerCase() === name.toLowerCase();
   });
 }
