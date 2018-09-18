9bd6974da252ffbd3b1f6bd04e198008777fff47
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-09-18 23:48:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function fuzzyMatch(drivers, letters) {
 }
 
 function matchName(drivers, name) {
-  return drivers.filter(function (driverName) {
-    return driverName === name;
+  return drivers.filter(function (driver) {
+    return driver.name.toLowerCase() === name.toLowerCase();
   });
 }
